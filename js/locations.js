2655558e7c31acb70bf086489aded610def164e6
Efrain <5731936+fefroyo@users.noreply.github.com>
2021-09-06 17:07:59 +0200

    coords locations
    locations of google mapaa

---
+++ js/locations.js
@@ -0,0 +1,20 @@
+var paradas = [
+	{	"name": "can humet",
+		"imagen": "http://icons.iconarchive.com/icons/hektakun/pokemon/72/004-Charmander-icon.png",
+		"lat": 41.48216,"lng": 2.322943
+	}, {
+		"name": "plaza ocata",
+		// "imagen": "https://raw.githubusercontent.com/nepixl/hiRes_PkmIcons_TG-Discord/main/Discord_hiRes_noShiny_withBorder/pokemon_icon_"+val+"_00.png",
+		// "imagen": "https://raw.githubusercontent.com/nepixl/hiRes_PkmIcons_TG-Discord/main/Discord_hiRes_noShiny_withBorder/pokemon_icon_133_00.png",
+		"imagen": "pstop.png",
+		"lat": 41.481447,"lng": 2.322169
+	}, {
+		"name": "jardins milenari",
+		"imagen": "http://icons.iconarchive.com/icons/hektakun/pokemon/72/004-Charmander-icon.png",
+		"lat": 41.482539,"lng": 2.325211
+	}, {
+		"name": "Bondi Beach",
+		"imagen": "http://icons.iconarchive.com/icons/hektakun/pokemon/72/004-Charmander-icon.png",
+		"lat": -33.890542,"lng": 151.274856
+	}
+];
