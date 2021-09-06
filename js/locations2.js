32e969b0fe6e40201450f7bf2f4b5a1c2fb6ed4b
Efrain <5731936+fefroyo@users.noreply.github.com>
2021-09-06 17:44:30 +0200

    locations2
    locations salamanca argentona

---
+++ js/locations2.js
@@ -0,0 +1,93 @@
+var paradas = [
+  {
+    "latitude": 40.872229,
+    "longitude": -6.74703,
+    "nombre": "Iglesia de Ahigal de los Aceiteros",
+    "city": "ahigal de los aceiteros",
+    "imagen": "https://lh6.ggpht.com/Mf5IyYj3cVZri2kSHE-XWYgEuCMcKgz8NOnCaJAmBbsfRi4RsLjCRamsCqMuSZZFFBYOMdmYj0L0E2W2yAVD"
+  },
+  {
+    "latitude": 40.909189,
+    "longitude": -6.749926,
+    "nombre": "Ayuntamiento de la redonda",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/ByTWTOGDX2nni_7CSXRgPiKPnfKet3ULlYA6GegcTThSdQnNz1QrfJtMumH4uPpyHL-sCHXB0euBxKWw69PQjf0nbQ"
+  },
+  {
+    "latitude": 40.90916,
+    "longitude": -6.750262,
+    "nombre": "Parque infantil 1998",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/4nZwiq_35yPqBRn05W_bBeZTi8tCO1rTaMPhxfR8AJlAZld6l2smAIhiBApodnhTq_Jhz3bFGTCCipaX8oIp8tjNyA8"
+  },
+  {
+    "latitude": 40.910058,
+    "longitude": -6.749083,
+    "nombre": "Ermita del Nazareno",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/pNdrTUvrJru_qBzsg565v7irzxFVopCKWpSu6wORWacIypHwsLG38iGVYAzu_8pXS0qZMV-aNjUEffGYXMPpW3jKg-w"
+  },
+  {
+    "latitude": 40.909113,
+    "longitude": -6.747609,
+    "nombre": "Tierras de salamanca",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/7MILINeMo9MbWZc0jKJkYnRU4tOfjoSY9VsmgsNC35CKROsX3c-nD67UHobrDCPpq6wEKkcKt-cuISZqRkn-I9E7pQg"
+  },
+  {
+    "latitude": 40.908417,
+    "longitude": -6.747603,
+    "nombre": "Fuente del caño",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/SyBOZzOOxGHfnKenR8m9F40CDIZ-eCS5vY8YHxB-ULubBDfhu-LeXJlx8BLzHtUHUP88QLTuj99eUxRa7DATfspQD5s"
+  },
+  {
+    "latitude": 40.909836,
+    "longitude": -6.764863,
+    "nombre": "Fuente el Guijo",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/WhQLSAW7qjAKlwzsHSo3MO5YR42hULVG4X9tqLq6heUUGTbvZXYXDzoqgfBUjJO0UyIotI4LLvPC2TaHv4QUdMKLSw"
+  },
+  {
+    "latitude": 40.909411,
+    "longitude": -6.748814,
+    "nombre": "Fuente redonda",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/tVQN7SQi14r5Ef9s_l3rZf3zCdyhz5q1xiKCA9pHbZMX3Oj_Pa1vQaK6ZLG9zGMN7MG1X8gomL9wdqgg9Vk3x066MkI2"
+  },
+  {
+    "latitude": 40.906575,
+    "longitude": -6.749338,
+    "nombre": "Sendero de las piedras Magicas",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/HBH9Cxgs1Ilzs5mkhncaGOK2Cbv0he8m5JGfmfhRkNmFDtwsRs43xqpOU7e2-mlQWP8uEUAXiZ9b405Onukdsg7bcp0"
+  },
+  {
+    "latitude": 40.909722,
+    "longitude": -6.748996,
+    "nombre": "Cruz de piedra",
+    "city": "la redonda",
+    "imagen": "https://lh3.googleusercontent.com/3qyLDwwa0P2OlMQtIBpZrjmowrOxAhZNnFEXv4MR7OWRoW5knq2QGiLN9PniebCDh4KyIKyuClb-d2fR6uv-AS-XrpY"
+  },
+  {
+    "latitude": 41.557241,
+    "longitude": 2.376862,
+    "nombre": "can raimi",
+    "city": "argentona",
+    "imagen": ""
+  },
+  {
+    "latitude": 41.558456,
+    "longitude": 2.37762,
+    "nombre": "Football Field",
+    "city": "argentona",
+    "imagen": "https://lh3.googleusercontent.com/VUIkqveW03bJ6KIjVAd8s8KlXaym9TXPd4O586KVxDPOEWFyuS0WQmqbP7AK1p6Xf_zpx9hD-o32yqig4qXzWPk6Dagv"
+  },
+  {
+    "latitude": 41.558867,
+    "longitude": 2.377211,
+    "nombre": "Fuente de piedra Can Raimi",
+    "city": "argentona",
+    "imagen": "https://lh3.googleusercontent.com/6LsUUlbHXXt_Vt9gWanTskKkwmepE--tdzFUZfPrPtcvnTuHFGeIhKfxKNZA7nveXcV-rGc590mNBu6BV6boQkxeEiBMT3Pl_ytrpCM"
+  }
+];
